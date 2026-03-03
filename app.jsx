4112a47a2ac05cc22055212929b6c23e048489b8
NITIN GUPTA <SUPERFULL03213@GMAIL.COM>
2026-03-03 14:47:00 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (taskText) => {
+    const newTask = {
+      id: Date.now(),
+      text: taskText
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  return (
+    <div className="container">
+      <h1>Task Manager</h1>
+      <TaskForm addTask={addTask} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} />
+    </div>
+  );
+}
+
+export default App;
